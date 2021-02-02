95279c69789c1648b966a65feedf8c8b473d1001
faruk <farukyildirim.9595@gmail.com>
2021-02-02 00:42:57 +0000

    Code Cleanup

--- whatsapp-group-chat-analysis.py
+++ whatsapp-group-chat-analysis.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import re
 
-def startsWithDateAndTime(s):
+
+def starts_with_date_and_time(s):
     # regex pattern for date.(Works only for android. IOS Whatsapp export format is different. Will update the code soon
     s = ''.join(filter(None, s.split("[")))
     s = ''.join(filter(None, s.split("]")))
@@ -11,29 +12,32 @@ def startsWithDateAndTime(s):
         return True
     return False
 
-def getDataPoint(line):
+
+def get_data_point(line):
     n = 2
-    splitLine = line.split()
-    dateTime = ' '.join(splitLine[:n])
-    dateTime = ''.join(filter(None, dateTime.split("[")))
-    dateTime = ''.join(filter(None, dateTime.split("]")))
-    authorMessage = ' '.join(splitLine[n:])
-    if ": " in authorMessage:
+    split_line = line.split()
+    date_time = ' '.join(split_line[:n])
+    date_time = ''.join(filter(None, date_time.split("[")))
+    date_time = ''.join(filter(None, date_time.split("]")))
+    author_message = ' '.join(split_line[n:])
+    if ": " in author_message:
         n = 1
-        splitted = authorMessage.split(": ")
-        author = ''.join(splitted[:n])
-        message = ''.join(splitted[n:])
+        split_message = author_message.split(": ")
+        author = ''.join(split_message[:n])
+        message = ''.join(split_message[n:])
     else:
         author = "-"
-        message = authorMessage
-    return dateTime, author, message
+        message = author_message
+    return date_time, author, message
 
-parsedData = [] # List to keep track of data so it can be used by a Pandas dataframe
+
+parsedData = []  # List to keep track of data so it can be used by a Pandas dataframe
 # Upload your file here
-conversationPath = '__PATH__' # chat file
+conversationPath = 'E:\Desktop\Project\Survivor_THY_Teknik_chat.txt'  # chat file
 
 with open(conversationPath, encoding="utf-8") as fp:
-    fp.readline() # Skipping first line of the file because contains information related to something about end-to-end encryption
+    fp.readline()  # Skipping first line of the file because contains information related to something about
+    # end-to-end encryption
     messageBuffer = []
     dateTime, author = None, None
     while True:
@@ -41,16 +45,16 @@ with open(conversationPath, encoding="utf-8") as fp:
         if not line:
             break
         line = line.strip()
-        if startsWithDateAndTime(line):
+        if starts_with_date_and_time(line):
             if len(messageBuffer) > 0:
                 parsedData.append([dateTime, author, ' '.join(messageBuffer)])
             messageBuffer.clear()
-            dateTime, author, message = getDataPoint(line)
+            dateTime, author, message = get_data_point(line)
             messageBuffer.append(message)
         else:
             messageBuffer.append(line)
 
-df = pd.DataFrame(parsedData, columns=['Date - Time', 'Author', 'Message']) # Initialising a pandas Dataframe.
+df = pd.DataFrame(parsedData, columns=['Date - Time', 'Author', 'Message'])  # Initialising a pandas Dataframe.
 df["Date - Time"] = pd.to_datetime(df["Date - Time"])
 
 df.to_excel("wp_group_chat.xlsx", index=False)
